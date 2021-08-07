b387341686724c7284e7181a56d3f2bdd7ecec6a
Jawad Ul Hassan <intelligentjawad@gmail.com>
2021-08-07 16:27:43 +0500

    New pages added with some revisions

--- JS/script.js
+++ JS/script.js
@@ -368,3 +368,71 @@ btn.on('click', function (e) {
   e.preventDefault();
   $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+// Accordion Action
+const accordionItem = document.querySelectorAll('.accordion-item');
+const filterItems = document.querySelector('.filters-items');
+
+accordionItem.forEach(el =>
+  el.addEventListener('click', () => {
+    // if (el.classList.contains('active')) {
+    //   el.classList.remove('active');
+    // } else {
+    accordionItem.forEach(el2 => el2.classList.remove('active'));
+    el.classList.add('active');
+  })
+);
+
+$('a.like-button').on('click', function (e) {
+  $(this).toggleClass('liked');
+
+  setTimeout(() => {
+    $(e.target).removeClass('liked');
+  }, 1000);
+});
+
+// Get the modal
+const modal = document.querySelector('#myModal');
+
+// Get the button that opens the modal
+const btn12 = document.querySelectorAll('.filter-mobile-menu');
+
+// Get the <span> element that closes the modal
+const closeModal = document.getElementsByClassName('close')[0];
+const body1 = document.querySelector('body');
+
+for (let i = 0; i < btn12.length; i++) {
+  btn12[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    body1.style.overflowY = 'hidden';
+  });
+}
+
+// When the user clicks the button, open the modal
+btn12.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+// closeModal.onclick = function () {
+//   modal.style.display = 'none';
+//   body1.style.overflowY = 'auto';
+// };
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+// const thumbs = document.querySelector('.thumb-img').children;
+
+function changeImage(event) {
+  document.querySelector('.pro-img').src = event.children[0].src;
+
+  for (let i = 0; i < thumbs.length; i++) {
+    thumbs[i].classList.remove('active');
+  }
+  event.classList.add('active');
+}
